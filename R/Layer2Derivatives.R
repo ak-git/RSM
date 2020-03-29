c34d35c8073d743122f4f8f5a4b2f81b934ae177
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-03-29 22:43:21 +0300

    dR / dRho chart

--- R/Layer2Derivatives.R
+++ R/Layer2Derivatives.R
@@ -1,20 +1,20 @@
+mp <- function(ls) {
+  return(1 / abs(ls))
+}
+
+MP <- function(ls, n, hToL) {
+  ls ^ 2 + (4 * n * hToL) ^ 2 -> result
+  return(1 / sqrt(result))
+}
+
 dRdHLR <- function(k, hToL, sToL) {
-  mp <- function(ls) {
-    return(1 / abs(ls))
-  }
-
-  MP <- function(ls, n) {
-    ls ^ 2 + (4 * n * hToL) ^ 2 -> result
-    return(1 / sqrt(result))
-  }
-
   sum(sapply(1:4096, function(x) {
-    x ^ 2 * k ^ x * (-MP(1 - sToL, x) ^ 3 + MP(1 + sToL, x) ^ 3) -> result
+    x ^ 2 * k ^ x * (-MP(1 - sToL, x, hToL) ^ 3 + MP(1 + sToL, x, hToL) ^ 3) -> result
     return(result)
   })) -> nom
 
   sum(sapply(1:4096, function(x) {
-    k ^ x * (MP(1 - sToL, x) - MP(1 + sToL, x)) -> result
+    k ^ x * (MP(1 - sToL, x, hToL) - MP(1 + sToL, x, hToL)) -> result
     return(result)
   })) -> den
 
@@ -24,18 +24,47 @@ dRdHLR <- function(k, hToL, sToL) {
   return(R)
 }
 
-source('R/rsm.R')
-exp(seq(log(0.001), log(1), length.out = 100)) -> hToL
-sapply(hToL, function(x) dRdHLR(rhoToK(10, 1), x, 1 / 2)) -> y
-sapply(hToL, function(x) dRdHLR(rhoToK(2, 1), x, 1 / 2)) -> y2
-par(mar = c(5, 6, 1, 1))
-plot(hToL, y, type = "l", lwd = 2, log = "xy",
-    xlab = expression(bold(frac(h, L))),
-    ylab = expression(bold(frac(partialdiff~R, partialdiff~h)))
-)
-lines(hToL, y2, type = "l", lwd = 2, lty = 2)
-legend("topleft", box.col = 'white', inset = 0.01,
-    title = expression(bold(frac(s, L) == frac(1, 2))),
-    legend = c(expression(bold(frac(rho[1], rho[2]) == 10)), expression(bold(frac(rho[1], rho[2]) == 2))),
-    lty=c(1, 2), lwd = c(2, 2), horiz = F, y.intersp = 2
-)
+dRdrho2N <- function(k, hToL, sToL) {
+  sum(sapply(1:4096, function(x) {
+    x * k ^ (x - 1) * (MP(1 - sToL, x, hToL) - MP(1 + sToL, x, hToL)) -> result
+    return(result)
+  })) -> nom
+
+  sum(sapply(1:4096, function(x) {
+    k ^ x * (MP(1 - sToL, x, hToL) - MP(1 + sToL, x, hToL)) -> result
+    return(result)
+  })) -> den
+
+  (mp(1 - sToL) - mp(1 + sToL)) + 2 * den -> den
+
+  (1 - k ^ 2) * nom / den -> R
+  return(R)
+}
+
+dRdrho1N <- function(k, hToL, sToL) {
+  return(1 - dRdrho2N(k, hToL, sToL))
+}
+
+plotdRdrhoN <- function() {
+  source('R/rsm.R')
+  exp(seq(log(0.01), log(1), length.out = 100)) -> hToL
+  sapply(hToL, function(x) dRdrho1N(rhoToK(2, 1), x, 1 / 2)) -> y1
+  sapply(hToL, function(x) dRdrho2N(rhoToK(2, 1), x, 1 / 2)) -> y2
+  par(mar = c(5, 6, 1, 1))
+  plot(hToL, y1, type = "l", lwd = 2, log = "x", ylim = c(0, 1), lty = 1,
+       xlab = expression(bold(frac(h, L))),
+       ylab = expression(bold(frac(partialdiff~R, partialdiff~rho[i]) %.% frac(rho[i], R))),
+  )
+  lines(hToL, y2, type = "l", lwd = 2, lty = 5)
+  abline(h = c(0:3), col = 'black', lty = 1, lwd = 0.5)
+  legend("right", inset = 0.01, box.col = 'white',
+         title = expression(bold(list(frac(s, L) == frac(1, 2), phantom(0)~frac(rho[1], rho[2]) == 2))),
+         legend = c(
+           expression(bold(frac(partialdiff~R, partialdiff~rho[1]) %.% frac(rho[1], R))),
+           expression(bold(frac(partialdiff~R, partialdiff~rho[2]) %.% frac(rho[2], R)))
+         ),
+         lty = c(1, 5), lwd = c(2, 2), horiz = F, y.intersp = 2
+  )
+}
+
+plotdRdrhoN()
--- tests/testthat/test-Layer2Derivatives.R
+++ tests/testthat/test-Layer2Derivatives.R
@@ -13,3 +13,23 @@ test_that("dRdHLR", {
   ((R2 - R1) / dhmm) * (lmm / R1) -> der
   expect_equal(round(dRdHLR(0.8, 0.1, 0.3), 3), round(der, 3))
 })
+
+test_that("dRdrho1N", {
+  0.01 -> dx
+  20 -> lmm
+  9 -> rho1
+  layer2Ohms(rho1 = rho1, rho2 = 1, hmm = lmm * 0.2, smm = lmm * 0.5, lmm = lmm) -> R1
+  layer2Ohms(rho1 = rho1 + dx, rho2 = 1, hmm = lmm * 0.2, smm = lmm * 0.5, lmm = lmm) -> R2
+  ((R2 - R1) / dx) * (rho1 / R1) -> der
+  expect_equal(round(dRdrho1N(-0.8, 0.2, 0.5), 3), round(der, 3))
+})
+
+test_that("dRdrho2N", {
+  0.01 -> dx
+  20 -> lmm
+  1 -> rho2
+  layer2Ohms(rho1 = 9, rho2 = rho2, hmm = lmm * 0.2, smm = lmm * 0.5, lmm = lmm) -> R1
+  layer2Ohms(rho1 = 9, rho2 = rho2 + dx, hmm = lmm * 0.2, smm = lmm * 0.5, lmm = lmm) -> R2
+  ((R2 - R1) / dx) * (rho2 / R1) -> der
+  expect_equal(round(dRdrho2N(-0.8, 0.2, 0.5), 3), round(der, 3))
+})
