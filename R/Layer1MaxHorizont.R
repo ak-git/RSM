884f75f158f61f57eadaf7ec0a2b8d18543c33d1
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-05-31 21:13:40 +0300

    Графики горизонта событий

--- R/Layer1MaxHorizont.R
+++ R/Layer1MaxHorizont.R
@@ -16,13 +16,18 @@ abline(h = c(0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8), col = 'black', lty = 1, lw
 abline(v = seq(0, 1, 0.2), col = 'black', lty = 1, lwd = 0.5)
 abline(v = 1 / 3, col = 'black', lty = 2, lwd = 1.5)
 
-text(0.61, 1.5, expression(bold(delta ~ L == '0,001')), pos = 4)
-
 deltaL <- 0.01
 sapply(sToL, function (x) (hToL_Horizont(x, deltaL))) -> y2
 lines(sToL, y2, type = "l", lwd = 2, lty = 5)
-text(0.61, 0.7, expression(bold(delta ~ L == '0,01')), pos = 4)
 text(1 / 3, 0.45, expression(bold(frac(s, L) == frac(1, 3))), pos = 2, srt = 0)
+
+legend("topright", inset = 0.01, box.col = 'black',
+       legend = c(
+         expression(bold(delta ~ L == '0,001')),
+         expression(bold(delta ~ L == '0,01'))
+       ),
+       lty = c(1, 5), lwd = c(1, 1), horiz = F, y.intersp = 4, x.intersp = 2
+)
 
 exp(seq(log(0.001), log(0.1), length.out = 100)) -> deltaL
 sapply(deltaL, function (x) (hToL_Horizont(1 / 3, x))) -> y
@@ -30,6 +35,16 @@ plot(deltaL, y, type = "l", lwd = 2, ylim = c(0.4, 1.8), log = "xy",
      xlab = expression(bold(delta ~ L)),
      ylab = expression(bold(frac(h, L)))
 )
-text(0.03, 0.98, expression(bold(frac(s, L) == frac(1, 3))), pos = 1)
 abline(v = c(0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1), col = 'black', lty = 1, lwd = 0.5)
 abline(h = c(0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8), col = 'black', lty = 1, lwd = 0.5)
+
+sapply(deltaL, function (x) (hToL_Horizont(1 / 2, x))) -> y2
+lines(deltaL, y2, type = "l", lwd = 2, lty = 5)
+
+legend("topright", inset = 0.01, box.col = 'black',
+       legend = c(
+         expression(bold(frac(s, L) == frac(1, 3))),
+         expression(bold(frac(s, L) == frac(1, 2)))
+       ),
+       lty = c(1, 5), lwd = c(1, 1), horiz = F, y.intersp = 4, x.intersp = 2
+)
