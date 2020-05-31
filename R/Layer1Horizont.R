12e91c671c12c0f30007ed49045ae63270cc18f6
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-05-31 16:18:17 +0300

    Аналитическое выражение

--- R/Layer1Horizont.R
+++ R/Layer1Horizont.R
@@ -11,44 +11,36 @@ layer1RelativeRhoErrorIfLayer2Model <- function(rho1, rho2, hmm, smm, lmm) {
     actual <- rho1 * (1.0 + 2.0 / (mp(l - s) - mp(l + s)) * result)
     return(abs(actual - expected) / expected)
   }
-  return(min(err(rho1), err(rho2)))
+  return(err(rho1))
 }
 
-rho1ToRho2 <- 0.01
-sToL <- 0.1
-seq(0, 1, length.out = 100) -> hToL
+rho1ToRho2 <- 0.001
+sToL <- 1 / 3
+exp(seq(log(0.1), log(1), length.out = 100)) -> hToL
 sapply(hToL, function(x) (layer1RelativeRhoErrorIfLayer2Model(rho1ToRho2, 1.0, x, sToL, 1.0))) -> y1
 par(mar = c(5, 6, 1, 1))
-plot(hToL, y1, type = "l", lwd = 2, ylim = c(0, 1),
+plot(hToL, y1, type = "l", lwd = 2, log = "xy",
      xlab = expression(bold(frac(h, L))),
      ylab = expression(bold(delta ~ rho))
 )
 
-layer1RelativeRhoErrorIfLayer2Model2 <- function(hmm, smm, lmm) {
-  mmToSI(hmm) -> h
-  mmToSI(smm) -> s
-  mmToSI(lmm) -> l
-
+layer1RelativeRhoErrorIfLayer2Model2 <- function(hToL, sToL) {
   invHypot <- function(x, y) {
     result <- sqrt(x^2 + y^2)
     return(1 / result)
   }
 
-  sToL <- s / l
-  hToL <- h / l
   sum(sapply(1:MAX_SUM, function(x) (invHypot(1 - sToL, 4 * x * hToL) - invHypot(1 + sToL, 4 * x * hToL)))) -> result
   actual <- (1 - sToL^2) / sToL * result
   return(abs(actual))
 }
 
-sapply(hToL, function(x) (layer1RelativeRhoErrorIfLayer2Model2(x, sToL, 1.0))) -> y2
+library("VGAM")
+layer1RelativeRhoErrorIfLayer2Model3 <- function(hToL, sToL) {
+  return((1 - sToL^2) * zeta(3) / (32 * hToL^3))
+}
+
+sapply(hToL, function(x) (layer1RelativeRhoErrorIfLayer2Model2(x, sToL))) -> y2
 lines(hToL, y2, type = "l", lwd = 2, lty = 5)
-abline(h = 0.0, col = 'black', lty = 1, lwd = 0.5)
-legend("topright", box.col = 'white', inset = 0.2,
-       title = expression(bold(frac(s, L) == frac(1, 10))),
-       legend = c(
-         expression(bold(frac(rho[1], rho[2]) == 0.01)),
-         expression(bold(frac(rho[1], rho[2]) == 100))
-       ),
-       lty = c(1, 5), lwd = c(2, 2), horiz = F, y.intersp = 10
-)
+sapply(hToL, function(x) (layer1RelativeRhoErrorIfLayer2Model3(x, sToL))) -> y3
+lines(hToL, y3, type = "l", lwd = 2, lty = 6)
