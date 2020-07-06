295496680b67ecca642e278d8547311fe2322e9f
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-07-06 11:43:00 +0300

    Ближний и дальний горизонты событий

---
+++ R/Layer1Horizont-2.R
@@ -0,0 +1,69 @@
+source("R/rsm.R")
+
+apparentRho <- function(rho1 = 1, rho2 = Inf, hToL, sToL = 1 / 3) {
+  l <- 1
+  s <- sToL
+  h <- hToL
+  k <- rhoToK(rho1, rho2)
+
+  sum(sapply(1:MAX_SUM, function(x) ((k^x) * (MP(l - s, x, h) - MP(l + s, x, h))))) -> result
+  rhoApparent <- rho1 * (1.0 + 2.0 / (mp(l - s) - mp(l + s)) * result)
+  return(rhoApparent)
+}
+
+hToL_Horizont <- function(deltaL, k = 1) {
+  sum(sapply(1:MAX_SUM, function(n) (k^n / n^3))) -> result
+  return((abs(result / deltaL))^(1 / 3) / 6)
+}
+
+rho1 <- 1
+rho2 <- 2
+k <- rhoToK(rho1 = rho1, rho2 = rho2)
+
+exp(seq(log(0.01), log(2), length.out = 100)) -> hToL
+sapply(hToL, function(x) (apparentRho(hToL = x))) -> y1
+par(mar = c(5, 6, 1, 1), mfrow = c(1, 1))
+plot(hToL, y1, type = "l", lwd = 2, log = "xy", ylim = c(rho1, rho2),
+     xlab = expression(bold(frac(h, L))),
+     ylab = expression(bold(frac(rho[apparent], rho[1])))
+)
+
+sapply(hToL, function(x) (apparentRho(hToL = x, rho2 = rho2))) -> y2
+lines(hToL, y2, type = "l", lwd = 2, lty = 5)
+abline(h = c(1, rho2))
+
+dL <- 0.1
+L <- 30
+deltaL <- dL / L
+
+abline(v = hToL_Horizont(deltaL = deltaL), h = 6 * deltaL + 1, lty = 3, lwd = 2)
+abline(v = hToL_Horizont(deltaL = deltaL, k = k), h = 6 * deltaL + 1, lty = 3, lwd = 2)
+text(x = 0.1, y = 6 * deltaL + 1, expression(delta ~ rho[1] == frac(1 + over(s, L), over(s, L) %.% (1 - over(s, L))) %.% delta ~ L == 6 %.% delta ~ L == "2 %"),
+     pos = 3, srt = 0)
+text(x = 0.1, y = 6 * deltaL + 1 + 0.1, expression(delta ~ L == frac('0,1 mm', '30 mm')), pos = 3, srt = 0)
+
+text(x = hToL_Horizont(deltaL = deltaL, k = rhoToK(1, 2)) - 0.05, y = 1.2,
+     expression(frac(h, L) == frac(1, 6) %.% sqrt(frac(abs(sum(frac(k^n, n^3), n == 1, n)), delta ~ L), 3) ~ '= 0.785'),
+     pos = 3, srt = 90)
+text(x = hToL_Horizont(deltaL = deltaL) + 0.1, y = 1.2,
+     expression(frac(h, L) == frac(1, 6) %.% sqrt(frac(zeta(3), delta ~ L), 3) ~ '= 1,185'),
+     pos = 1, srt = 90)
+
+hToL_HorizontMin <- function(deltaL, k = 1) {
+  x <- 1 / 3
+  sum(sapply(1:MAX_SUM, function(n) (k^n * n^2))) -> r
+  r <- deltaL * (1 + k) / (1 - k) / r
+  r <- r * (1 - x) * (1 + x)^3 / 16 / x / (x^2 + 3)
+  return(sqrt(abs(r)))
+}
+
+abline(v = hToL_HorizontMin(deltaL = deltaL, k = k), h = rho2 * (1 - 6 * deltaL), lty = 3, lwd = 2)
+
+legend("topright", box.col = 'black',
+       title = expression(bold(frac(s, L) == frac(1, 3))),
+       legend = c(
+         expression(bold(rho[2] == infinity)),
+         expression(bold(frac(rho[1], rho[2]) == frac(1, 2)))
+       ),
+       lty = c(1, 5), lwd = c(1, 1), horiz = F
+)
--- R/formula1.R
+++ R/formula1.R
@@ -1,5 +1,5 @@
-k <- 0.8
-a <- 0.5
+k <- 0.1
+a <- 0.1
 
 f <- function(x) {
 
@@ -12,14 +12,23 @@ f <- function(x) {
   return(abs(result))
 }
 
-g <- function(x) {
+gHigh <- function(x) {
   sum(sapply(1:1024, function(n) (k^n / n^3))) -> result
   return(abs(result * 2 * a / x^3))
 }
 
-exp(seq(log(0.01), log(100), length.out = 100)) -> x
+gLow <- function(x) {
+  sum(sapply(1:1024, function(n) (k^n))) -> result
+  sum(sapply(1:1024, function(n) ((k^n) * (n^2)))) -> r2
+  r2 * x^2 / 2 * (1 / (1 - a)^3 - 1 / (1 + a)^3) -> r2
+  return(abs(2 * a * result / (1 - a^2) - r2))
+}
+
+exp(seq(log(0.01), log(10), length.out = 100)) -> x
 sapply(x, f) -> y
-sapply(x, g) -> y2
+sapply(x, gHigh) -> y2
+sapply(x, gLow) -> y3
 
 plot(x, y, type = "l", lwd = 2, log = "xy")
 lines(x, y2, type = "l", lwd = 2, lty = 5)
+lines(x, y3, type = "l", lwd = 2, lty = 6)
